27621ee86698342d3972712b7d87b0b2c3929849
upuaut <upuaut>
2002-05-01 14:37:15 +0000

    Moved the powerpc register definitions into beecrypt.gas.h

--- beecrypt.gas.h
+++ beecrypt.gas.h
@@ -79,4 +79,44 @@
 # endif
 #endif
 
+#if defined(OPTIMIZE_POWERPC)
+# if DARWIN
+#  define LOAD_ADDRESS(reg,var) lis reg,ha16(var); la reg,lo16(var)(reg)
+# else
+#  define LOAD_ADDRESS(reg,var) lis reg,var@ha; la reg,var@l(reg)
+#  define r0 %r0
+#  define r1 %r1
+#  define r2 %r2
+#  define r3 %r3
+#  define r4 %r4
+#  define r5 %r5
+#  define r6 %r6
+#  define r7 %r7
+#  define r8 %r8
+#  define r9 %r9
+#  define r10 %r10
+#  define r11 %r11
+#  define r12 %r12
+#  define r13 %r13
+#  define r14 %r14
+#  define r15 %r15
+#  define r16 %r16
+#  define r17 %r17
+#  define r18 %r18
+#  define r19 %r19
+#  define r20 %r20
+#  define r21 %r21
+#  define r22 %r22
+#  define r23 %r23
+#  define r24 %r24
+#  define r25 %r25
+#  define r26 %r26
+#  define r27 %r27
+#  define r28 %r28
+#  define r29 %r29
+#  define r30 %r30
+#  define r31 %r31
+# endif
+#endif
+
 #endif
