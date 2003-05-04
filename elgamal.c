46818f9c026004670898357f597c6fbe8a7ba336
upuaut <upuaut>
2003-05-04 08:56:59 +0000

    Modified doxygen comments.

--- elgamal.c
+++ elgamal.c
@@ -19,18 +19,7 @@
  
 /*!\file elgamal.c
  * \brief ElGamal algorithm.
- *
- * For more information on this algorithm, see:
- *  "Handbook of Applied Cryptography",
- *  11.5.2: "The ElGamal signature scheme", p. 454-459
- *
- * Two of the signature variants in Note 11.70 are described.
- *
- * \todo Explore the possibility of using simultaneous multiple exponentiation,
- *       as described in HAC, 14.87 (iii).
- *
  * \author Bob Deblier <bob.deblier@pandora.be>
- * \ingroup DL_m DL_elgamal_m
  */
 
 #define BEECRYPT_DLL_EXPORT
@@ -45,30 +34,6 @@
 # include <malloc.h>
 #endif
 
-/*!\addtogroup DL_elgamal_m
- * \{
- */
-
-/*!\fn int elgv1sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s)
- * \brief The raw ElGamal signing funcion, variant 1.
- *
- * Signing equations:
- *
- * \li \f$r=g^{k}\ \textrm{mod}\ p\f$
- * \li \f$s=k^{-1}(h(m)-xr)\ \textrm{mod}\ (p-1)\f$
- *
- * \param p The prime.
- * \param n The reducer mod (p-1).
- * \param g The generator.
- * \param rgc The pseudo-random generat
- * \param hm The hash to be signed.
- * \param x The private key value.
- * \param r The signature's \e r value.
- * \param s The signature's \e r value.
- *
- * \retval 0 on success.
- * \retval -1 on failure.
- */
 int elgv1sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s)
 {
 	register size_t size = p->size;
@@ -106,30 +71,6 @@ int elgv1sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomG
 	return -1;
 }
 
-/*!\fn int elgv1vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
- * \brief The raw ElGamal verification funcion, variant 1.
- *
- * Verifying equations:
- *
- * \li Check \f$0<r<p\f$ and \f$0<s<(p-1)\f$
- * \li \f$v_1=y^{r}r^{s}\ \textrm{mod}\ p\f$
- * \li \f$v_2=g^{h(m)}\ \textrm{mod}\ p\f$
- * \li Check \f$v_1=v_2\f$
- *
- * \param p The prime.
- * \param n The reducer mod (p-1).
- * \param g The generator.
- * \param hm The hash to be signed.
- * \param y The public key value.
- * \param r The signature's \e r value.
- * \param s The signature's \e r value.
- *
- * \warning The return type of this function should be a boolean, but since
- *          that type isn't as portable, an int is used.
- *
- * \retval 1 on success.
- * \retval 0 on failure.
- */
 int elgv1vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
 {
 	register size_t size = p->size;
@@ -174,26 +115,6 @@ int elgv1vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const m
 	return 0;
 }
 
-/*!\fn int elgv3sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s)
- * \brief The raw ElGamal signing funcion, variant 3.
- *
- * Signing equations:
- *
- * \li \f$r=g^{k}\ \textrm{mod}\ p\f$
- * \li \f$s=xr+kh(m)\ \textrm{mod}\ (p-1)\f$
- *
- * \param p The prime.
- * \param n The reducer mod (p-1).
- * \param g The generator.
- * \param rgc The pseudo-random generat
- * \param hm The hash to be signed.
- * \param x The private key value.
- * \param r The signature's \e r value.
- * \param s The signature's \e r value.
- *
- * \retval 0 on success.
- * \retval -1 on failure.
- */
 int elgv3sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s)
 {
 	register size_t size = p->size;
@@ -227,30 +148,6 @@ int elgv3sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomG
 	return -1;
 }
 
-/*!\fn int elgv3vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
- * \brief The raw ElGamal verification funcion, variant 3.
- *
- * Verifying equations:
- *
- * \li Check \f$0<r<p\f$ and \f$0<s<(p-1)\f$
- * \li \f$v_1=g^{s}\ \textrm{mod}\ p\f$
- * \li \f$v_2=y^{r}r^{h(m)}\ \textrm{mod}\ p\f$
- * \li Check \f$v_1=v_2\f$
- *
- * \param p The prime.
- * \param n The reducer mod (p-1).
- * \param g The generator.
- * \param hm The hash to be signed.
- * \param y The public key value.
- * \param r The signature's \e r value.
- * \param s The signature's \e r value.
- *
- * \warning The return type of this function should be a boolean, but since
- *          that type isn't as portable, an int is used.
- *
- * \retval 1 on success.
- * \retval 0 on failure.
- */
 int elgv3vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
 {
 	register size_t size = p->size;
--- elgamal.h
+++ elgamal.h
@@ -18,10 +18,23 @@
  */
 
 /*!\file elgamal.h
- * \brief ElGamal algorithm, headers.
+ * \brief ElGamal algorithm.
+ *
+ * For more information on this algorithm, see:
+ *  "Handbook of Applied Cryptography",
+ *  11.5.2: "The ElGamal signature scheme", p. 454-459
+ *
+ * Two of the signature variants in Note 11.70 are implemented.
+ *
+ * \todo Implement ElGamal encryption and decryption.
+ *
+ * \todo Explore the possibility of using simultaneous multiple exponentiation,
+ *       as described in HAC, 14.87 (iii).
+ *
  * \author Bob Deblier <bob.deblier@pandora.be>
  * \ingroup DL_m DL_elgamal_m
  */
+
 #ifndef _ELGAMAL_H
 #define _ELGAMAL_H
 
@@ -31,13 +44,95 @@
 extern "C" {
 #endif
 
+/*!\fn int elgv1sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g,
+randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r,
+ mpnumber* s)
+ * \brief This function performs raw ElGamal signing, variant 1.
+ *
+ * Signing equations:
+ *
+ * \li \f$r=g^{k}\ \textrm{mod}\ p\f$
+ * \li \f$s=k^{-1}(h(m)-xr)\ \textrm{mod}\ (p-1)\f$
+ *
+ * \param p The prime.
+ * \param n The reducer mod (p-1).
+ * \param g The generator.
+ * \param rgc The pseudo-random generat
+ * \param hm The hash to be signed.
+ * \param x The private key value.
+ * \param r The signature's \e r value.
+ * \param s The signature's \e s value.
+ * \retval 0 on success.
+ * \retval -1 on failure.
+ */
 BEECRYPTAPI
 int elgv1sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext*, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s);
+
+/*!\fn int elgv1vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
+ * \brief This function performs raw ElGamal verification, variant 1.
+ *
+ * Verifying equations:
+ *
+ * \li Check \f$0<r<p\f$ and \f$0<s<(p-1)\f$
+ * \li \f$v_1=y^{r}r^{s}\ \textrm{mod}\ p\f$
+ * \li \f$v_2=g^{h(m)}\ \textrm{mod}\ p\f$
+ * \li Check \f$v_1=v_2\f$
+ *
+ * \param p The prime.
+ * \param n The reducer mod (p-1).
+ * \param g The generator.
+ * \param hm The hash to be signed.
+ * \param y The public key value.
+ * \param r The signature's \e r value.
+ * \param s The signature's \e s value.
+ * \retval 1 on success.
+ * \retval 0 on failure.
+ */
 BEECRYPTAPI
 int elgv3sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext*, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s);
 
+/*!\fn int elgv3sign(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, randomGeneratorContext* rgc, const mpnumber* hm, const mpnumber* x, mpnumber* r, mpnumber* s)
+ * \brief This function performs raw ElGamal signing, variant 3.
+ *
+ * Signing equations:
+ *
+ * \li \f$r=g^{k}\ \textrm{mod}\ p\f$
+ * \li \f$s=xr+kh(m)\ \textrm{mod}\ (p-1)\f$
+ *
+ * \param p The prime.
+ * \param n The reducer mod (p-1).
+ * \param g The generator.
+ * \param rgc The pseudo-random generat
+ * \param hm The hash to be signed.
+ * \param x The private key value.
+ * \param r The signature's \e r value.
+ * \param s The signature's \e s value.
+ * \retval 0 on success.
+ * \retval -1 on failure.
+ */
 BEECRYPTAPI
 int elgv1vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s);
+
+/*!\fn int elgv3vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s)
+ * \brief This function performs raw ElGamal verification, variant 3.
+ *
+ * Verifying equations:
+ *
+ * \li Check \f$0<r<p\f$ and \f$0<s<(p-1)\f$
+ * \li \f$v_1=g^{s}\ \textrm{mod}\ p\f$
+ * \li \f$v_2=y^{r}r^{h(m)}\ \textrm{mod}\ p\f$
+ * \li Check \f$v_1=v_2\f$
+ *
+ * \param p The prime.
+ * \param n The reducer mod (p-1).
+ * \param g The generator.
+ * \param hm The hash to be signed.
+ * \param y The public key value.
+ * \param r The signature's \e r value.
+ * \param s The signature's \e s value.
+ * \retval 1 on success.
+ * \retval 0 on failure.
+ */
 BEECRYPTAPI
 int elgv3vrfy(const mpbarrett* p, const mpbarrett* n, const mpnumber* g, const mpnumber* hm, const mpnumber* y, const mpnumber* r, const mpnumber* s);
 
