195d6b2d9d6f939a5632e1d36012c5aa9dab4f4d
upuaut <upuaut>
2003-05-29 12:04:45 +0000

    Fixed problem introduced in blockEncryptCBC when I made index i unsigned; the current solution is cleaner.

--- blockmode.c
+++ blockmode.c
@@ -84,7 +84,6 @@ int blockEncryptCBC(const blockCipher* bc, blockCipherParam* bp, uint32_t* dst, 
 
 		bc->encrypt(bp, dst, dst);
 
-		dst += blockwords;
 		src += blockwords;
 
 		nblocks--;
@@ -92,18 +91,21 @@ int blockEncryptCBC(const blockCipher* bc, blockCipherParam* bp, uint32_t* dst, 
 		while (nblocks > 0)
 		{
 			for (i = 0; i < blockwords; i++)
-				dst[i] = src[i] ^ dst[i-blockwords];
+				dst[i+blockwords] = src[i] ^ dst[i];
+
+			dst += blockwords;
 
 			bc->encrypt(bp, dst, dst);
 
-			dst += blockwords;
 			src += blockwords;
 
 			nblocks--;
 		}
 
+		dst -= blockwords;
+
 		for (i = 0; i < blockwords; i++)
-			fdback[i] = dst[i-blockwords];
+			fdback[i] = dst[i];
 	}
 	return 0;
 }
