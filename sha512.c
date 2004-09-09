fdac56d35fb57bfef34506475f5ba93c7cd5d1ba
upuaut <upuaut>
2004-09-09 06:38:35 +0000

    Added generic SSE optimization. Fixed L/LL constant declarations.

--- sha512.c
+++ sha512.c
@@ -32,62 +32,120 @@
 #include "beecrypt/sha512.h"
 #include "beecrypt/endianness.h"
 
+#ifdef OPTIMIZE_SSE
+# include <xmmintrin.h>
+#endif
+
 /*!\addtogroup HASH_sha512_m
  * \{
  */
 
 static const uint64_t k[80] = {
-	0x428a2f98d728ae22U, 0x7137449123ef65cdU,
-	0xb5c0fbcfec4d3b2fU, 0xe9b5dba58189dbbcU,
-	0x3956c25bf348b538U, 0x59f111f1b605d019U,
-	0x923f82a4af194f9bU, 0xab1c5ed5da6d8118U,
-	0xd807aa98a3030242U, 0x12835b0145706fbeU,
-	0x243185be4ee4b28cU, 0x550c7dc3d5ffb4e2U,
-	0x72be5d74f27b896fU, 0x80deb1fe3b1696b1U,
-	0x9bdc06a725c71235U, 0xc19bf174cf692694U,
-	0xe49b69c19ef14ad2U, 0xefbe4786384f25e3U,
-	0x0fc19dc68b8cd5b5U, 0x240ca1cc77ac9c65U,
-	0x2de92c6f592b0275U, 0x4a7484aa6ea6e483U,
-	0x5cb0a9dcbd41fbd4U, 0x76f988da831153b5U,
-	0x983e5152ee66dfabU, 0xa831c66d2db43210U,
-	0xb00327c898fb213fU, 0xbf597fc7beef0ee4U,
-	0xc6e00bf33da88fc2U, 0xd5a79147930aa725U,
-	0x06ca6351e003826fU, 0x142929670a0e6e70U,
-	0x27b70a8546d22ffcU, 0x2e1b21385c26c926U,
-	0x4d2c6dfc5ac42aedU, 0x53380d139d95b3dfU,
-	0x650a73548baf63deU, 0x766a0abb3c77b2a8U,
-	0x81c2c92e47edaee6U, 0x92722c851482353bU,
-	0xa2bfe8a14cf10364U, 0xa81a664bbc423001U,
-	0xc24b8b70d0f89791U, 0xc76c51a30654be30U,
-	0xd192e819d6ef5218U, 0xd69906245565a910U,
-	0xf40e35855771202aU, 0x106aa07032bbd1b8U,
-	0x19a4c116b8d2d0c8U, 0x1e376c085141ab53U,
-	0x2748774cdf8eeb99U, 0x34b0bcb5e19b48a8U,
-	0x391c0cb3c5c95a63U, 0x4ed8aa4ae3418acbU,
-	0x5b9cca4f7763e373U, 0x682e6ff3d6b2b8a3U,
-	0x748f82ee5defb2fcU, 0x78a5636f43172f60U,
-	0x84c87814a1f0ab72U, 0x8cc702081a6439ecU,
-	0x90befffa23631e28U, 0xa4506cebde82bde9U,
-	0xbef9a3f7b2c67915U, 0xc67178f2e372532bU,
-	0xca273eceea26619cU, 0xd186b8c721c0c207U,
-	0xeada7dd6cde0eb1eU, 0xf57d4f7fee6ed178U,
-	0x06f067aa72176fbaU, 0x0a637dc5a2c898a6U,
-	0x113f9804bef90daeU, 0x1b710b35131c471bU,
-	0x28db77f523047d84U, 0x32caab7b40c72493U,
-	0x3c9ebe0a15c9bebcU, 0x431d67c49c100d4cU,
-	0x4cc5d4becb3e42b6U, 0x597f299cfc657e2aU,
-	0x5fcb6fab3ad6faecU, 0x6c44198c4a475817U
+	#if (SIZEOF_UNSIGNED_LONG == 8) || !HAVE_UNSIGNED_LONG_LONG
+	0x428a2f98d728ae22UL, 0x7137449123ef65cdUL,
+	0xb5c0fbcfec4d3b2fUL, 0xe9b5dba58189dbbcUL,
+	0x3956c25bf348b538UL, 0x59f111f1b605d019UL,
+	0x923f82a4af194f9bUL, 0xab1c5ed5da6d8118UL,
+	0xd807aa98a3030242UL, 0x12835b0145706fbeUL,
+	0x243185be4ee4b28cUL, 0x550c7dc3d5ffb4e2UL,
+	0x72be5d74f27b896fUL, 0x80deb1fe3b1696b1UL,
+	0x9bdc06a725c71235UL, 0xc19bf174cf692694UL,
+	0xe49b69c19ef14ad2UL, 0xefbe4786384f25e3UL,
+	0x0fc19dc68b8cd5b5UL, 0x240ca1cc77ac9c65UL,
+	0x2de92c6f592b0275UL, 0x4a7484aa6ea6e483UL,
+	0x5cb0a9dcbd41fbd4UL, 0x76f988da831153b5UL,
+	0x983e5152ee66dfabUL, 0xa831c66d2db43210UL,
+	0xb00327c898fb213fUL, 0xbf597fc7beef0ee4UL,
+	0xc6e00bf33da88fc2UL, 0xd5a79147930aa725UL,
+	0x06ca6351e003826fUL, 0x142929670a0e6e70UL,
+	0x27b70a8546d22ffcUL, 0x2e1b21385c26c926UL,
+	0x4d2c6dfc5ac42aedUL, 0x53380d139d95b3dfUL,
+	0x650a73548baf63deUL, 0x766a0abb3c77b2a8UL,
+	0x81c2c92e47edaee6UL, 0x92722c851482353bUL,
+	0xa2bfe8a14cf10364UL, 0xa81a664bbc423001UL,
+	0xc24b8b70d0f89791UL, 0xc76c51a30654be30UL,
+	0xd192e819d6ef5218UL, 0xd69906245565a910UL,
+	0xf40e35855771202aUL, 0x106aa07032bbd1b8UL,
+	0x19a4c116b8d2d0c8UL, 0x1e376c085141ab53UL,
+	0x2748774cdf8eeb99UL, 0x34b0bcb5e19b48a8UL,
+	0x391c0cb3c5c95a63UL, 0x4ed8aa4ae3418acbUL,
+	0x5b9cca4f7763e373UL, 0x682e6ff3d6b2b8a3UL,
+	0x748f82ee5defb2fcUL, 0x78a5636f43172f60UL,
+	0x84c87814a1f0ab72UL, 0x8cc702081a6439ecUL,
+	0x90befffa23631e28UL, 0xa4506cebde82bde9UL,
+	0xbef9a3f7b2c67915UL, 0xc67178f2e372532bUL,
+	0xca273eceea26619cUL, 0xd186b8c721c0c207UL,
+	0xeada7dd6cde0eb1eUL, 0xf57d4f7fee6ed178UL,
+	0x06f067aa72176fbaUL, 0x0a637dc5a2c898a6UL,
+	0x113f9804bef90daeUL, 0x1b710b35131c471bUL,
+	0x28db77f523047d84UL, 0x32caab7b40c72493UL,
+	0x3c9ebe0a15c9bebcUL, 0x431d67c49c100d4cUL,
+	0x4cc5d4becb3e42b6UL, 0x597f299cfc657e2aUL,
+	0x5fcb6fab3ad6faecUL, 0x6c44198c4a475817UL
+	#else
+	0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL,
+	0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
+	0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL,
+	0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
+	0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
+	0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
+	0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL,
+	0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
+	0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL,
+	0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+	0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL,
+	0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
+	0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL,
+	0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
+	0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
+	0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
+	0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL,
+	0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
+	0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL,
+	0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+	0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL,
+	0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
+	0xd192e819d6ef5218ULL, 0xd69906245565a910ULL,
+	0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
+	0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
+	0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
+	0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL,
+	0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
+	0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL,
+	0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+	0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL,
+	0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
+	0xca273eceea26619cULL, 0xd186b8c721c0c207ULL,
+	0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
+	0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
+	0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
+	0x28db77f523047d84ULL, 0x32caab7b40c72493ULL,
+	0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
+	0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL,
+	0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL
+	#endif
 };
 
 static const uint64_t hinit[8] = {
-	0x6a09e667f3bcc908U,
-	0xbb67ae8584caa73bU,
-	0x3c6ef372fe94f82bU,
-	0xa54ff53a5f1d36f1U,
-	0x510e527fade682d1U,
-	0x9b05688c2b3e6c1fU,
-	0x1f83d9abfb41bd6bU,
-	0x5be0cd19137e2179U
+	#if (SIZEOF_UNSIGNED_LONG == 8) || !HAVE_UNSIGNED_LONG_LONG
+	0x6a09e667f3bcc908UL,
+	0xbb67ae8584caa73bUL,
+	0x3c6ef372fe94f82bUL,
+	0xa54ff53a5f1d36f1UL,
+	0x510e527fade682d1UL,
+	0x9b05688c2b3e6c1fUL,
+	0x1f83d9abfb41bd6bUL,
+	0x5be0cd19137e2179UL
+	#else
+	0x6a09e667f3bcc908ULL,
+	0xbb67ae8584caa73bULL,
+	0x3c6ef372fe94f82bULL,
+	0xa54ff53a5f1d36f1ULL,
+	0x510e527fade682d1ULL,
+	0x9b05688c2b3e6c1fULL,
+	0x1f83d9abfb41bd6bULL,
+	0x5be0cd19137e2179ULL
+	#endif
 };
 
 const hashFunction sha512 = { "SHA-512", sizeof(sha512Param), 128, 64, (hashFunctionReset) sha512Reset, (hashFunctionUpdate) sha512Update, (hashFunctionDigest) sha512Digest };
@@ -107,31 +165,87 @@ int sha512Reset(register sha512Param* sp)
 	return 0;
 }
 
-#define R(x,s)  ((x) >> (s))
-#define S(x,s) ROTR64(x, s)
+#ifdef OPTIMIZE_SSE
 
-#define CH(x,y,z) ((x&(y^z))^z)
-#define MAJ(x,y,z) (((x|y)&z)|(x&y))
-#define SIG0(x)	(S(x,28) ^ S(x,34) ^ S(x,39))
-#define SIG1(x)	(S(x,14) ^ S(x,18) ^ S(x,41))
-#define sig0(x) (S(x,1) ^ S(x,8) ^ R(x,7))
-#define sig1(x) (S(x,19) ^ S(x,61) ^ R(x,6))
+# define R(x,s) _mm_srli_si64(x,s)
+# define S(x,s) _m_pxor(_mm_srli_si64(x,s),_mm_slli_si64(x,64-(s)))
+# define CH(x,y,z) _m_pxor(_m_pand(x,_m_pxor(y,z)),z)
+# define MAJ(x,y,z) _m_por(_m_pand(_m_por(x,y),z),_m_pand(x,y))
+# define SIG0(x) _m_pxor(_m_pxor(S(x,28),S(x,34)),S(x,39))
+# define SIG1(x) _m_pxor(_m_pxor(S(x,14),S(x,18)),S(x,41))
+# define sig0(x) _m_pxor(_m_pxor(S(x,1),S(x,8)),R(x,7))
+# define sig1(x) _m_pxor(_m_pxor(S(x,19),S(x,61)),R(x,6))
 
-#define ROUND(a,b,c,d,e,f,g,h,w,k)	\
+# define ROUND(a,b,c,d,e,f,g,h,w,k) \
+	temp = _mm_add_si64(h, _mm_add_si64(_mm_add_si64(SIG1(e), CH(e,f,g)), _mm_add_si64((__m64) k, (__m64) w))); \
+	h = _mm_add_si64(temp, _mm_add_si64(SIG0(a), MAJ(a,b,c))); \
+	d = _mm_add_si64(d, temp)
+
+#else
+
+# define R(x,s) ((x) >> (s))
+# define S(x,s) ROTR64(x, s)
+
+# define CH(x,y,z) ((x&(y^z))^z)
+# define MAJ(x,y,z) (((x|y)&z)|(x&y))
+# define SIG0(x) (S(x,28) ^ S(x,34) ^ S(x,39))
+# define SIG1(x) (S(x,14) ^ S(x,18) ^ S(x,41))
+# define sig0(x) (S(x,1) ^ S(x,8) ^ R(x,7))
+# define sig1(x) (S(x,19) ^ S(x,61) ^ R(x,6))
+
+# define ROUND(a,b,c,d,e,f,g,h,w,k)	\
 	temp = h + SIG1(e) + CH(e,f,g) + k + w;	\
 	h = temp + SIG0(a) + MAJ(a,b,c);	\
 	d += temp
 
+#endif
+
 #ifndef ASM_SHA512PROCESS
 void sha512Process(register sha512Param* sp)
 {
+	#ifdef OPTIMIZE_SSE
+	# if HAVE_UNSIGNED_LONG_LONG
+	static const uint64_t MASK = 0x00FF00FF00FF00FFULL;
+	# else
+	static const uint64_t MASK = 0x00FF00FF00FF00FFUL;
+	# endif
+
+	__m64 a, b, c, d, e, f, g, h, temp;
+	register __m64* w;
+	register byte t;
+
+	w = (__m64*) sp->data;
+	t = 16;
+	while (t--)
+	{
+		temp = *w;
+		*(w++) = _m_pxor(
+				_mm_slli_si64(_m_pshufw(_m_pand(temp, (__m64) MASK), 27), 8),
+				_m_pshufw(_m_pand(_mm_srli_si64(temp, 8), (__m64) MASK), 27)
+			);
+	}
+
+	t = 64;
+	while (t--)
+	{
+		temp = _mm_add_si64(_mm_add_si64(sig1(w[-2]), w[-7]), _mm_add_si64(sig0(w[-15]), w[-16]));
+		*(w++) = temp;
+	}
+
+	w = (__m64*) sp->data;
+
+	a = (__m64) sp->h[0]; b = (__m64) sp->h[1]; c = (__m64) sp->h[2]; d = (__m64) sp->h[3];
+	e = (__m64) sp->h[4]; f = (__m64) sp->h[5]; g = (__m64) sp->h[6]; h = (__m64) sp->h[7];
+
+	#else
+
 	register uint64_t a, b, c, d, e, f, g, h, temp;
 	register uint64_t *w;
 	register byte t;
 
-	#if WORDS_BIGENDIAN
+	# if WORDS_BIGENDIAN
 	w = sp->data + 16;
-	#else
+	# else
 	w = sp->data;
 	t = 16;
 	while (t--)
@@ -139,7 +253,7 @@ void sha512Process(register sha512Param* sp)
 		temp = swapu64(*w);
 		*(w++) = temp;
 	}
-	#endif
+	# endif
 
 	t = 64;
 	while (t--)
@@ -152,6 +266,7 @@ void sha512Process(register sha512Param* sp)
 
 	a = sp->h[0]; b = sp->h[1]; c = sp->h[2]; d = sp->h[3];
 	e = sp->h[4]; f = sp->h[5]; g = sp->h[6]; h = sp->h[7];
+	#endif
 
 	ROUND(a,b,c,d,e,f,g,h,w[ 0],k[ 0]);
 	ROUND(h,a,b,c,d,e,f,g,w[ 1],k[ 1]);
@@ -234,6 +349,17 @@ void sha512Process(register sha512Param* sp)
 	ROUND(c,d,e,f,g,h,a,b,w[78],k[78]);
 	ROUND(b,c,d,e,f,g,h,a,w[79],k[79]);
 
+	#ifdef OPTIMIZE_SSE
+	sp->h[0] = (uint64_t) _mm_add_si64((__m64) sp->h[0], a);
+	sp->h[1] = (uint64_t) _mm_add_si64((__m64) sp->h[1], b);
+	sp->h[2] = (uint64_t) _mm_add_si64((__m64) sp->h[2], c);
+	sp->h[3] = (uint64_t) _mm_add_si64((__m64) sp->h[3], d);
+	sp->h[4] = (uint64_t) _mm_add_si64((__m64) sp->h[4], e);
+	sp->h[5] = (uint64_t) _mm_add_si64((__m64) sp->h[5], f);
+	sp->h[6] = (uint64_t) _mm_add_si64((__m64) sp->h[6], g);
+	sp->h[7] = (uint64_t) _mm_add_si64((__m64) sp->h[7], h);
+	_mm_empty();
+	#else
 	sp->h[0] += a;
 	sp->h[1] += b;
 	sp->h[2] += c;
@@ -242,6 +368,7 @@ void sha512Process(register sha512Param* sp)
 	sp->h[5] += f;
 	sp->h[6] += g;
 	sp->h[7] += h;
+	#endif
 }
 #endif
 
