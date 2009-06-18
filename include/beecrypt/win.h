579a4af53c5e14cbddacc7f71026d695e5faea9c
upuaut <upuaut>
2009-06-18 19:55:37 +0000

    Cleaned up.

--- include/beecrypt/win.h
+++ include/beecrypt/win.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2000, 2001, 2002, 2005 X-Way Rights BV
+ * Copyright (c) 2009 Bob Deblier
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -28,20 +29,20 @@
 #define _REENTRANT
 
 #if !defined(_WIN32_WINNT)
-#define _WIN32_WINNT 0x0400
+# define _WIN32_WINNT 0x0400
 #endif
 
 #include <windows.h>
 
+#define WORDS_BIGENDIAN		0
+
 #if __MWERKS__
 # if __INTEL__
-#  define WORDS_BIGENDIAN		0
 # else
 #  error Unknown CPU type in MetroWerks CodeWarrior
 # endif
 #elif defined(_MSC_VER)
 # if defined(_M_IX86)
-#  define WORDS_BIGENDIAN		0
 #  define ROTL32(x, s) _rotl(x, s)
 #  define ROTR32(x, s) _rotr(x, s)
 # else
@@ -52,56 +53,56 @@
 #endif
 
 #if defined(_MSC_VER) || __MWERKS__
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
-#define HAVE_ASSERT_H			1
+# define HAVE_ASSERT_H			1
 
-#define HAVE_ERRNO_H			1
-#define HAVE_CTYPE_H			1
-#define HAVE_FCNTL_H			1
-#define HAVE_TIME_H				1
+# define HAVE_ERRNO_H			1
+# define HAVE_CTYPE_H			1
+# define HAVE_FCNTL_H			1
+# define HAVE_TIME_H			1
 
-#define HAVE_SYS_TYPES_H		0
-#define HAVE_SYS_TIME_H			0
+# define HAVE_SYS_TYPES_H		0
+# define HAVE_SYS_TIME_H		0
 
-#define HAVE_THREAD_H			0
-#define HAVE_SYNCH_H			0
-#define HAVE_PTHREAD_H			0
-#define HAVE_SEMAPHORE_H		0
+# define HAVE_THREAD_H			0
+# define HAVE_SYNCH_H			0
+# define HAVE_PTHREAD_H			0
+# define HAVE_SEMAPHORE_H		0
 
-#define HAVE_TERMIO_H			0
-#define HAVE_SYS_AUDIOIO_H		0
-#define HAVE_SYS_IOCTL_H		0
-#define HAVE_SYS_SOUNDCARD_H	0
+# define HAVE_TERMIO_H			0
+# define HAVE_SYS_AUDIOIO_H		0
+# define HAVE_SYS_IOCTL_H		0
+# define HAVE_SYS_SOUNDCARD_H	0
 
-#define HAVE_GETTIMEOFDAY		0
-#define HAVE_GETHRTIME			0
+# define HAVE_GETTIMEOFDAY		0
+# define HAVE_GETHRTIME			0
 
-#define HAVE_DEV_TTY			0
-#define HAVE_DEV_AUDIO			0
-#define HAVE_DEV_DSP			0
-#define HAVE_DEV_RANDOM			0
-#define HAVE_DEV_URANDOM		0
-#define HAVE_DEV_TTY			0
-
+# define HAVE_DEV_TTY			0
+# define HAVE_DEV_AUDIO			0
+# define HAVE_DEV_DSP			0
+# define HAVE_DEV_RANDOM		0
+# define HAVE_DEV_URANDOM		0
+# define HAVE_DEV_TTY			0
 #else
-#error Not set up for this compiler
+# error Not set up for this compiler
 #endif
 
 #if __MWERKS__
-#define HAVE_SYS_STAT_H			0
 
-#define HAVE_LONG_LONG			1
-#define HAVE_UNSIGNED_LONG_LONG	1
+# define HAVE_SYS_STAT_H			0
 
-#define HAVE_64_BIT_INT			1
-#define HAVE_64_BIT_UINT		1
+# define HAVE_LONG_LONG				1
+# define HAVE_UNSIGNED_LONG_LONG	1
 
-#define SIZEOF_SIZE_T			4 /* not sure about this one */
-#define SIZEOF_UNSIGNED_LONG	4
+# define HAVE_64_BIT_INT			1
+# define HAVE_64_BIT_UINT			1
 
+# define SIZEOF_SIZE_T				4 /* not sure about this one */
+# define SIZEOF_UNSIGNED_LONG		4
+ 
 typedef char		int8_t;
 typedef short		int16_t;
 typedef long		int32_t;
@@ -113,16 +114,17 @@ typedef unsigned long		uint32_t;
 typedef unsigned long long	uint64_t;
 
 #elif defined(_MSC_VER)
-#define HAVE_SYS_STAT_H			1
 
-#define HAVE_LONG_LONG			0
-#define HAVE_UNSIGNED_LONG_LONG	0
+# define HAVE_SYS_STAT_H			1
 
-#define HAVE_64_BIT_INT			1
-#define HAVE_64_BIT_UINT		1
+# define HAVE_LONG_LONG				0
+# define HAVE_UNSIGNED_LONG_LONG	0
 
-#define SIZEOF_SIZE_T			4
-#define SIZEOF_UNSIGNED_LONG	4
+# define HAVE_64_BIT_INT			1
+# define HAVE_64_BIT_UINT			1
+
+# define SIZEOF_SIZE_T				4
+# define SIZEOF_UNSIGNED_LONG		4
 
 typedef signed char	int8_t;
 typedef signed short int16_t;
